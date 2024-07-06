d05797cd2a17e1094f05fca330c83577d1b9ede7
Adarsh71002 <116730632+Adarsh71002@users.noreply.github.com>
2024-07-06 23:39:38 +0530

    Initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Eat and Fun</div>
+          <div className="right">
+            <p>Kuruksherta , Haryana</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Adarsh</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Our Team.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
